56ddfe06253528be8668a559661e81e848ec271c
uihyeon1229 <101084941+uihyeon1229@users.noreply.github.com>
2022-06-17 10:29:27 +0900

    프로그래머스 완주하지못한선수

---
+++ src/main/java/com/example/algorithm/Programmers/완주하지못한선수.java
@@ -0,0 +1,31 @@
+package com.example.algorithm.Programmers;
+
+import java.util.HashMap;
+
+public class 완주하지못한선수 {
+    public String solution(String[] participant, String[] completion) {
+        HashMap<String ,Integer> player = new HashMap<String ,Integer>();
+
+        for(String name : participant){
+            if(player.get(name)==null){
+                player.put(name,1);
+            }else{
+                player.put(name,player.get(name)+1);
+            }
+        }
+
+        for(String name : completion){
+            if(player.get(name)!=0){
+                player.put(name, player.get(name)-1);
+            }
+        }
+
+        for(String key : player.keySet()){
+            if(player.get(key)!=0){
+                return key;
+            }
+        }
+
+        return "";
+    }
+}
